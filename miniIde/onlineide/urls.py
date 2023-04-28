ea23ef74861aba8d4e98a7a1f41773ab7571ed4b
praduman03 <113344907+praduman03@users.noreply.github.com>
2023-04-28 15:06:04 +0530

    Add files via upload

---
+++ miniIde/onlineide/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register("user", views.UserViewSet)
+router.register("submit", views.SubmissionsViewSet)
+
+urlpatterns = [
+    path("", views.hello_world),
+    path("login/", views.LoginView.as_view()),
+    path("register/", views.register),
+]
+
+urlpatterns += router.urls
